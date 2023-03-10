7f04e0d9338a036a48c24441aef7cd49b966c3cc
John <john@Johns-MBP.lan1>
2023-03-10 09:53:45 -0600

    readme formatting

--- app/readme.js
+++ app/readme.js
@@ -5,8 +5,7 @@ const task_readme = () => {
   const posts = pages.reduce((string,file) => {
     const date = file.date ? file.date.toLocaleString('en-US', { weekday: 'short', day: 'numeric', month: 'short', year: 'numeric' }) : ``;
     return `${string}
-${date}<br/>
-[${file.title}](${base_url}/${file.folder}/)
+* ${date} [${file.title}](${base_url}/${file.folder}/)
 `},``);
   const markdown = `# John Mills
 I mostly post minimal experiments, tips, and tricks. Keep up-to-date with me and subscribe to my [RSS Feed](${base_url}/johnmills.rss).
